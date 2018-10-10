62371b636294c2c2ff9e199d4406e1f6d1b2109f
Ahn Jaechan <jaechan5256@gmail.com>
2018-10-11 02:06:46 +0900

    refactor(vm): Changed mappages function to non-static. Added prototype declaration to trap.c

--- trap.c
+++ trap.c
@@ -14,6 +14,8 @@ extern uint vectors[];  // in vectors.S: array of 256 entry pointers
 struct spinlock tickslock;
 uint ticks;
 
+int mappages(pde_t *pgdir, void *va, uint size, uint pa, int perm);
+
 void
 tvinit(void)
 {
